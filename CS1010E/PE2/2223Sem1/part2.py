081f0b7f713ac59f3c65eb69d35a9ab1e82a870d
kulakaka <simonfans0928@gmail.com>
2023-11-06 01:07:24 +0800

    part2 finish
    Signed-off-by: kulakaka <simonfans0928@gmail.com>

--- CS1010E/PE2/2223Sem1/part2.py
+++ CS1010E/PE2/2223Sem1/part2.py
@@ -1,7 +1,4 @@
 
-def prince_map(filename,prince):
- pass
-
 
 def readmap_n(filename):
     f = open(filename)
@@ -27,40 +24,62 @@ def readmap(filename):
 def island_area(filename,prince):
     stack=[]
     visited=[]
-    area = 1
+    area = 0
     map = readmap(filename)
     for x in range(len(map)):
         for y in range(len(map[x])):
             if map[x][y]==prince:
                 r=x
                 c=y 
-
-    DFS_island(map,[r,c],stack,visited)
-
+    stack.append([r,c])
+    area = DFS_island(map,[r,c],stack,visited)
+    return len(area)
 def DFS_island(map,startpoint,stack,visited):
+    
     r,c = startpoint
-   
-    if [r,c] not in stack and map[r+1][c] =='.':
+    if not stack:
+        return visited
+
+    if [r,c] not in visited and map[r+1][c] =='.':
         stack.append([r+1,c])
-        #area+=1
-    if [r,c] not in stack and map[r-1][c]=='.':
+    if [r,c] not in visited and map[r-1][c]=='.':
         stack.append([r-1,c])
-        #area+=1
-    if [r,c] not in stack and map[r][c+1]=='.':
+    if [r,c] not in visited and map[r][c+1]=='.':
         stack.append([r,c+1])
-        #area+=1
-    if [r,c] not in stack and map[r-1][c-1]=='.':
+    if [r,c] not in visited and map[r][c-1]=='.':
         stack.append([r,c-1])
-        #area+=1
     
-    visited.append([r,c])
+    if[r,c] not in visited:
+        visited.append([r,c])
 
     
-    return   DFS_island(map,[i[0],i[1]],stack,visited)
+    return DFS_island(map,stack[0],stack[1:],visited)
+    
 
 
 
-    return len(visited)
+#print(island_area("map1.txt","C"))
 
-print(island_area("map1.txt","B"))
+
+def prince_map(filename,prince):
+    stack=[]
+    visited=[]
+    area = 0
+    map = readmap(filename)
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y]==prince:
+                r=x
+                c=y 
+    stack.append([r,c])
+    area = DFS_island(map,[r,c],stack,visited)
+    row=[]
+    col=[]
+    for i in area:
+        row.append(i[0])
+        col.append(i[1])
+    
+    return crop_map(filename,min(row),max(row)+1,min(col),max(col)+1)
+
+#print(prince_map('map1.txt','A'))
 
