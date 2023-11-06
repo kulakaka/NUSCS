fa5fe6b9fd0303da04f3980c1c5140722638b3eb
kulakaka <simonfans0928@gmail.com>
2023-11-06 23:15:14 +0800

    c
    Signed-off-by: kulakaka <simonfans0928@gmail.com>

--- CS1010E/PE2/2122Sem1/part1.py
+++ CS1010E/PE2/2122Sem1/part1.py
@@ -5,11 +5,26 @@ def per_cipher_i(s,n):
     for i in range(len(s)//n+1):
         output+=reversed(s[:n])
         s=s[n:]
-        print(s)
     for i in output:
         pp+=i
     return pp
 
 
-print(per_cipher_i('PE Part 1 is supposed to be easy',7))
+#print(per_cipher_i('PE Part 1 is supposed to be easy',7))
 
+
+def per_cipher_r(s,n):
+    if not s:
+        return ''
+    else:
+        a = s[:n]
+        return a[::-1]+per_cipher_r(s[n:],n)
+    
+#print(per_cipher_r(per_cipher_r('12345678910',3),3))
+
+
+def per_cipher_o(s,n):
+    return s[:n][::-1]+per_cipher_r(s[n:],n)
+
+
+print(per_cipher_o('12345678910',3))
